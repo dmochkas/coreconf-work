5fd90e28fffa9fe6057f3a870aac6d7bec1e3727
lroliver03 <lucas@oliver.adm.br>
2026-04-22 17:11:16 +0200

    Implemented simple CORECONF simulation (datasotre generated on the fly, hard-coded data)

--- src/device.py
+++ src/device.py
@@ -1,61 +1,163 @@
-import asyncio
-import aiocoap
-# import json
+import sys
+import os
+
+import datetime
+import random
+import pprint
+import json
 import cbor2 as cbor
-import struct
-
-from common import defs
-
+import pycoreconf
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+SID_FILE = "basic-sensor.sid"
+SID_PATH = os.path.join(os.path.dirname(__file__), "model", SID_FILE)
+
+def generateFullData():
+  sensors = [
+    {
+      "type": "temperature",
+      "id": 0,
+      "enabled": False,
+      "unit": "°C",
+      "quantity": {
+        "value": 0
+      },
+      "timestamp": 0
+    },
+    {
+      "type": "pressure",
+      "id": 1,
+      "enabled": False,
+      "unit": "kPa",
+      "quantity": {
+        "value": 0
+      },
+      "timestamp": 0
+    },
+    {
+      "type": "light",
+      "id": 2,
+      "enabled": False,
+      "unit": "lux",
+      "quantity": {
+        "value": 0
+      },
+      "timestamp": 0
+    }
+  ]
+
+  config = {
+    "basic-sensor:characteristics": {
+      "name": "Basic Sensor",
+      "version": "1.0",
+      "identifier": "basicsensor0"
+    },
+    "basic-sensor:sensors": {
+      "sensor": sensors
+    }
+  }
+
+  return config
+
 class CoAPClient:
-  def __init__(self, host, port=defs.PORT):
-    self.host = host
-    self.port = port
-    self.protocol = None
+  def __init__(self, config, datastore):
+    logger.info("[CoAPClient] Creating CoAPClient instance...")
 
-  async def connect(self):
-    self.protocol = await aiocoap.Context.create_client_context()
-    logger.info(f"CoAP client ready for {self.host}:{self.port}")
-
-  async def fetch(self, path: str|None = None, request: aiocoap.Message|None = None):
-    if path is None and request is None:
-      logger.error("FETCH error: No path or request given!")
-      return {"error": "No path or request"}
+    self.config = config
+    self.ds = datastore
     
-    if request is None:
-      uri = f"coap://{self.host}:{self.port}/{path}"
-      request = aiocoap.Message(code=aiocoap.FETCH, uri=uri)
+    logger.info("[CoAPClient] CoAPClient instance created!")
 
+  def GET(self, path: str):
+    """
+    CoAP GET operation on specified path path.
+    """
+
+    # Get resource
+    logger.info(f"[CoAPClient] Attempting to GET resource in path {path}...")
     try:
-      response = await self.protocol.request(request).response
-      logger.info(f"FETCH {path}: {response.code}")
-
-      if response.code.is_successful():
-        return cbor.loads(response.payload)
-      else:
-        return {"error": str(response.code)}
-
+      entry = self.ds[path]
+      return entry
     except Exception as e:
-      logger.error(f"FETCH error: {e}")
-      return {"error": str(e)}
+      logger.error("[CoAPClient] GET error")
+      logger.exception(e)
+      return None
 
-async def main():
-  client = CoAPClient(defs.HOST, defs.PORT)
-  await client.connect()
+def main():
 
-  uri = f"coap://{defs.HOST}:{defs.PORT}/{defs.SIDS['temperature:interface/sensor']}"
-  request = aiocoap.Message(
-    code=aiocoap.FETCH,
-    uri=uri,
-    payload=defs.SENSOR_NAME.encode()
-  )
-  print(f"Getting resource {uri.encode()}")
-  resources = await client.fetch(request=request)
-  print(f"Resource response: {resources}")
+  # Title
+  print("=" * 60)
+  print("CORECONF implementation of RPN calculator")
+  print("=" * 60)
 
-if __name__ == "__main__":
-  asyncio.run(main())
+  # Load SID file to pycoreconf model
+  print("\nLoading SID file...")
+  try:
+    ccm = pycoreconf.CORECONFModel(SID_PATH)
+  except Exception as e:
+    logger.exception(e)
+    sys.exit(1)
+  print("SID file loaded!")
+  input("Press Enter to continue...")
+
+  # Generate and print data
+  print("\nGenerating sample data...")
+  config_data = generateFullData()
+  print("Data generated! Shown below:")
+  pprint.pprint(config_data)
+  input("Press Enter to continue...")
+
+  # Dump into JSON string
+  print("\nDumping data into JSON string...")
+  try:
+    json_data = json.dumps(config_data, indent=2)
+  except Exception as e:
+    logger.exception(e)
+    sys.exit(1)
+  print("Data dumped into JSON string!")
+  print(json_data)
+  print(f"JSON data length: {len(json_data)} bytes")
+  input("Press Enter to continue...")
+
+  # Dump to CBOR string
+  print("\nDumping data into CBOR format through CORECONF...")
+  try:
+    cbor_data = ccm.toCORECONF(json_data)
+  except Exception as e:
+    logger.exception(e)
+    sys.exit(1)
+  print("CBOR string generated!")
+  print(f"CBOR data: {cbor_data.hex()}")
+  print(f"CBOR data length: {len(cbor_data)} bytes")
+  input("Press Enter to continue...")
+
+  print("\nLoading CBOR data into datastore...")
+  ds = ccm.create_datastore(cbor_data=cbor_data)
+  print("Datastore loaded!")
+
+  xpath_key = "/sensors/sensor"
+  print(f"\nReading datastore keys with path {xpath_key}")
+  try:
+    entry = ds[xpath_key]
+    print("Complete entry retrieved:")
+    pprint.pprint(entry)
+  except Exception as e:
+    logger.exception(e)
+    sys.exit(1)
+  input("Press Enter to continue...")
+
+  print("Entering CoAP test loop...")
+  client = CoAPClient(ccm, ds)
+  running = True
+  while len((xpath_key := input("\nEnter resource path: "))) > 0:
+    entry = client.GET(xpath_key)
+    print("Complete entry retrieved:")
+    pprint.pprint(entry)
+  
+  print("Exited loop. End program.")
+
+if __name__=="__main__":
+  main()
