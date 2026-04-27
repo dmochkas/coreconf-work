c89205dde7b62135fa43d769cf1f5e1e3984cbe5
lroliver03 <lucas@oliver.adm.br>
2026-04-27 15:18:08 +0200

    Created basic CORECONF client and server scripts with aiocoap

--- src/app.py
+++ src/app.py
@@ -1,44 +1,125 @@
 import asyncio
 import aiocoap
 import aiocoap.resource as resource
-# import json
+import json
 import cbor2 as cbor
+import pprint
 import pycoreconf
 
-from common import defs
+from base import CoAPBase
 
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-sid_file = "./src/model/temperature.sid"
-ccm = pycoreconf.CORECONFModel(sid_file, model_description_file=None)
+def generateFullData():
+  sensors = [
+    {
+      "type": "temperature",
+      "id": 0,
+      "enabled": False,
+      "unit": "°C",
+      "quantity": {
+        "value": 0
+      },
+      "timestamp": 0
+    },
+    {
+      "type": "pressure",
+      "id": 1,
+      "enabled": False,
+      "unit": "kPa",
+      "quantity": {
+        "value": 0
+      },
+      "timestamp": 0
+    },
+    {
+      "type": "light",
+      "id": 2,
+      "enabled": False,
+      "unit": "lux",
+      "quantity": {
+        "value": 0
+      },
+      "timestamp": 0
+    }
+  ]
 
-class ExampleResource(resource.Resource):
-  def exampleTask(self):
-    return b"task done!"
+  config = {
+    "basic-sensor:characteristics": {
+      "name": "Basic Sensor",
+      "version": "1.0",
+      "identifier": "basicsensor0"
+    },
+    "basic-sensor:sensors": {
+      "sensor": sensors
+    }
+  }
+
+  return config
+
+class CoAPServer(CoAPBase):
+  model = None
+  datastore = None
+  
+  @classmethod
+  def init(cls):
+    cls.model = pycoreconf.CORECONFModel(cls.sid_file)
+
+    config_data = generateFullData()
+    cbor_data = cls.model.toCORECONF(config_data)
+
+    cls.datastore = cls.model.create_datastore(cbor_data=cbor_data)
+
+class DatastoreResource(resource.Resource):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
 
   async def render_fetch(self, request: aiocoap.Message) -> aiocoap.Message:
-    logger.info(f"FETCH request from client {request.remote}")
-    logger.info(f"Payload: {request.payload}")
+    logger.info(f"[DatastoreResource.render_fetch] FETCH request from client {request.remote}")
+    payload = cbor.loads(request.payload)
+    logger.info(f"[DatastoreResource.render_fetch] Payload: {payload}")
 
-    if request.payload.decode() == defs.SENSOR_NAME:
-      payload_object = {60002: self.exampleTask()}
-      logger.info(f"FETCH payload: {payload_object}")
-      payload = cbor.dumps(payload_object)
-    else:
-      logger.error("FETCH error: Invalid payload")
-      payload = cbor.dumps({60002: "Invalid payload"})
+    response = []
+    for i, instance in enumerate(payload):
+      # If instance is only SID
+      if not isinstance(instance, (list, tuple)):
+        xpath = CoAPServer.datastore._create_xpath(instance)
+      # If instance is SID and keys
+      elif len(instance) > 1:
+        xpath = CoAPServer.datastore._create_xpath(instance[0], keys=instance[1:])
+      
+      full_xpath = f"{CoAPServer.module_name}:{xpath[1:]}"
+      logger.info(f"[DatastoreResource.render_fetch] Payload instance {i} has XPath {full_xpath}")
 
-    return aiocoap.Message(payload=payload, content_format=42)
+      instance_response = CoAPServer.datastore[xpath]
+      logger.info(f"[DatastoreResource.render_fetch] Obtained response for instance {i}. Printing.")
+      pprint.pprint(instance_response)
+
+      json_instance_response = json.dumps({full_xpath: instance_response})
+      logger.info(f"[DatastoreResource.render_fetch] Generated JSON instance response for instance {i}. Printing.")
+      pprint.pprint(json_instance_response)
+      
+      cbor_instance_response = CoAPServer.model.toCORECONF(json_instance_response)
+      response.append(cbor_instance_response)
+
+    cbor_response = cbor.dumps(response)
+    logger.info(f"[DatastoreResource.render_fetch] Full response obtained. Printing.")
+    logger.info(f"[DatastoreResource.render_fetch] Response: {cbor_response.hex()}")
+    logger.info(f"[DatastoreResource.render_fetch] Response size: {len(cbor_response)} bytes")
+
+    return aiocoap.Message(payload=cbor_response, content_format=142)
 
 async def main():
+  CoAPServer.init()
+
   root = resource.Site()
 
-  root.add_resource([defs.SIDS["temperature:interface/sensor"]], ExampleResource())
+  root.add_resource(["c"], DatastoreResource())
 
-  await aiocoap.Context.create_server_context(root, bind=(defs.HOST, defs.PORT))
+  await aiocoap.Context.create_server_context(root, bind=(CoAPServer.host, CoAPServer.port))
 
   await asyncio.get_running_loop().create_future()
 
--- src/device.py
+++ src/device.py
@@ -1,220 +1,130 @@
 import sys
 import os
 
-import datetime
-import random
-import pprint
+import aiocoap
+import asyncio
 import json
 import cbor2 as cbor
+import pprint
 import pycoreconf
+
+from base import CoAPBase
+
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-SID_FILE = "basic-sensor.sid"
-SID_PATH = os.path.join(os.path.dirname(__file__), "model", SID_FILE)
+class CoAPClient(CoAPBase):
+  def __init__(self, host: str, port: str|None=None):
+    self.host = host
+    self.port = port
+    self.protocol = None
 
-def generateFullData():
-  sensors = [
-    {
-      "type": "temperature",
-      "id": 0,
-      "enabled": False,
-      "unit": "°C",
-      "quantity": {
-        "value": 0
-      },
-      "timestamp": 0
-    },
-    {
-      "type": "pressure",
-      "id": 1,
-      "enabled": False,
-      "unit": "kPa",
-      "quantity": {
-        "value": 0
-      },
-      "timestamp": 0
-    },
-    {
-      "type": "light",
-      "id": 2,
-      "enabled": False,
-      "unit": "lux",
-      "quantity": {
-        "value": 0
-      },
-      "timestamp": 0
-    }
-  ]
+  async def init(self):
+    logger.info(f"[CoAPClient.init] Initializing CoAP client instance...")
 
-  config = {
-    "basic-sensor:characteristics": {
-      "name": "Basic Sensor",
-      "version": "1.0",
-      "identifier": "basicsensor0"
-    },
-    "basic-sensor:sensors": {
-      "sensor": sensors
-    }
-  }
-
-  return config
-
-class CoAPClient:
-  def __init__(self, config, datastore):
-    logger.info("[CoAPClient] Creating CoAPClient instance...")
-
-    self.config = config
-    self.ds = datastore
-    
-    logger.info("[CoAPClient] CoAPClient instance created!")
-
-  def GET(self, sid: int, keys: str):
-    """
-    CoAP GET operation on specified SID with keys.
-    """
-
-    # Get resource
-    logger.info(f"[CoAPClient] Attempting to GET resource in SID {sid}...")
+    # Attempt to create CORECONF model.
     try:
-      # Get XPath from SID and keys
-      logger.info(f"[CoAPClient] Creating XPath from SID and keys...")
-      if keys:
-        decoded_keys = cbor.loads(keys)
-      else:
-        decoded_keys = None
-      target_path = self.ds._create_xpath(sid, keys=decoded_keys)
-      
-      logger.info(f"[CoAPClient] Obtained path: {target_path}")
-      entry = self.ds[target_path]
-      return entry
-    except IndexError as e:
-      logger.error("Invalid path.")
-      return {"error": "invalid path"}
-    except Exception as e:
-      logger.error("[CoAPClient] GET error")
-      logger.exception(e)
-    return None
-
-def main():
-
-  # Title
-  print("=" * 60)
-  print("CORECONF implementation of basic sensor network")
-  print("=" * 60)
-
-  # Load SID file to pycoreconf model
-  print("\n\tLoading SID file...")
-  try:
-    ccm = pycoreconf.CORECONFModel(SID_PATH)
-  except Exception as e:
-    logger.exception(e)
-    sys.exit(1)
-  print("SID file loaded!")
-  input("Press Enter to continue...")
-
-  # Generate and print data
-  print("\n\tGenerating sample data...")
-  config_data = generateFullData()
-  print("Data generated! Shown below:")
-  pprint.pprint(config_data)
-  input("Press Enter to continue...")
-
-  # Dump into JSON string
-  print("\n\tDumping data into JSON string...")
-  try:
-    json_data = json.dumps(config_data, indent=2)
-  except Exception as e:
-    logger.exception(e)
-    sys.exit(1)
-  print("Data dumped into JSON string!")
-  print(json_data)
-  print(f"JSON data length: {len(json_data)} bytes")
-  input("Press Enter to continue...")
-
-  # Dump to CBOR string
-  print("\n\tDumping data into CBOR format through CORECONF...")
-  try:
-    cbor_data = ccm.toCORECONF(json_data)
-  except Exception as e:
-    logger.exception(e)
-    sys.exit(1)
-  print("CBOR string generated!")
-  print(f"CBOR data: {cbor_data.hex()}")
-  print(f"CBOR data length: {len(cbor_data)} bytes")
-  input("Press Enter to continue...")
-
-  # Load datastore with CBOR data
-  print("\n\tLoading CBOR data into datastore...")
-  ds = ccm.create_datastore(cbor_data=cbor_data)
-  print("Datastore loaded!")
-
-  # Testing datastore using XPath address
-  xpath_key = "/sensors/sensor"
-  print(f"\nReading datastore keys with path {xpath_key}")
-  try:
-    entry = ds[xpath_key]
-    print("Complete entry retrieved:")
-    pprint.pprint(entry)
-  except Exception as e:
-    logger.exception(e)
-    sys.exit(1)
-  input("Press Enter to continue...")
-
-  # Testing datastore using SID obtained from XPath address
-  print("\n\tTesting SIDs...")
-  for path_in in ["/characteristics/identifier", "/sensors/sensor", "/sensors/sensor[type='temperature'][id='0']/quantity/value"]:
-    try:
-      print(f"Getting SID from path: {path_in}")
-      
-      # Get SID from datastore _resolve_path method
-      target_sid, keys = ds._resolve_path(path_in)
-      print(f"Obtained SID {target_sid} with keys:")
-      pprint.pprint(keys)
-
-      # Get path back from datastore _create_xpath method
-      path_out = ds._create_xpath(target_sid, keys=keys)
-      print(f"Obtained path from SID: {path_out}")
-
-      if path_in == path_out:
-        print("Input and output paths match!")
-      else:
-        logger.error(f"Mismatched paths!\n\t In: {path_in}\n\tOut:{path_out}")
-
+      self.model = pycoreconf.CORECONFModel(self.sid_file)
     except Exception as e:
       logger.exception(e)
       sys.exit(1)
-  input("Press Enter to continue...")
+    logger.info(f"[CoAPClient.init] Created CORECONF model for SID file {self.sid_file}")
 
-  print("\n\tEntering CoAP test loop...")
-  client = CoAPClient(ccm, ds)
-  running = True
-  while len((xpath_key := input("\nEnter resource path: "))) > 0:
+    # Create client context for requests.
+    self.protocol = await aiocoap.Context.create_client_context()
 
-    # Get specific SID and keys from XPath
+    # Get module SID for module-wide datastore request
+    target_sid_1 = self.model.sids[f"/{self.module_name}:sensors/sensor"]
+    target_sid_2 = self.model.sids[f"/{self.module_name}:characteristics"]
+    target_sids = [target_sid_1, target_sid_2]
+
+    # Request full datastore
+    logger.info(f"[CoAPClient.init] FETCHing all sub-leafs from SIDs {target_sids}")
     try:
-      target_sid, keys = ds._resolve_path(xpath_key)
-    except Exception as e:
+      response = await self.FETCH("c?d=0", target_sids)
+    except TimeoutError as e:
+      logger.error("[CoAPClient.init] Failed to obtain datamodel response.")
       logger.exception(e)
-      print("\nContinuing...")
-      continue
 
-    print(f"SID obtained from path: {target_sid}")
-    print(f"Keys: {keys}")
-    if len(keys):
-      cbor_keys = cbor.dumps(keys)
-      print(f"Keys (CBOR encoding): {cbor_keys.hex()}")
+    logger.info(f"[CoAPClient.init] Datamodel response obtained!")
+    logger.info(f"[CoAPClient.init] Payload: {response.payload.hex()}")
+    logger.info(f"[CoAPClient.init] Payload size: {len(response.payload)} bytes")
+
+    full_response = {}
+    cbor_instance_list = cbor.loads(response.payload)
+    for i, cbor_instance in enumerate(cbor_instance_list):
+      logger.info(f"[CoAPClient.init] Decoding payload instance {i}!")
+      instance = json.loads(self.model.toJSON(cbor_instance))
+      pprint.pprint(instance)
+      full_response |= instance
+
+    logger.info(f"[CoAPClient.init] Full datamodel response obtained. Printing.")
+    pprint.pprint(full_response)
+
+    cbor_response = self.model.toCORECONF(json.dumps(full_response))
+
+    try:
+      self.ds = self.model.create_datastore(cbor_response)
+    except Exception as e:
+      logger.error("[CoAPClient.init] Failed to create datastore from response. Possible bad response!")
+      logger.exception(e)
+      sys.exit(1)
+
+    logger.info("[CoAPClient.init] Initialized client with datastore!")
+
+    logger.info("[CoAPClient.init] Printing full datastore for verification...")
+    pprint.pprint(self.ds)
+
+    logger.info("[CoAPClient.init] Client fully initialized!")
+
+  def _remote(self) -> str:
+    """
+    Resolve remote name.
+    """
+    return f"{self.host}:{self.port}" if self.port else self.host
+
+  def _request(self, path: str, payload: bytes) -> aiocoap.Message:
+    # Create initial aiocoap request message
+    req = aiocoap.Message(transport_tuning=aiocoap.Unreliable, code=aiocoap.FETCH, payload=payload)
+    
+    # Parse path and query, if query exists
+    if '?' in path:
+      p, q = path.split('?')
+      req.opt.uri_path = p
+      req.opt.uri_query = tuple(q.split('&'))
     else:
-      cbor_keys = None # If no keys, use None
-      print("Empty keys. No encoding necessary.")
+      req.opt.uri_path = path
 
-    entry = client.GET(target_sid, cbor_keys)
-    print("Complete entry retrieved:")
-    pprint.pprint(entry)
-  
-  print("Exited loop. End program.")
+    # Set remaining parameters
+    req.opt.content_format = 141 # application/yang-identifier+cbor-seq
+    req.opt.accept = 142 # application/yang-instances+cbor-seq
+    req.unresolved_remote = self._remote()
+    
+    return req
+
+  async def FETCH(self, xpath: str, payload):
+    logger.info(f"[CoAPClient.FETCH] Attempting to FETCH path {payload} in resource </{xpath}>")
+
+    cbor_payload = cbor.dumps(payload)
+    logger.info(f"[CoAPClient.FETCH] Payload: {cbor_payload.hex()}")
+    logger.info(f"[CoAPClient.FETCH] Payload size: {len(cbor_payload)}")
+
+    request = self._request(xpath, cbor_payload)
+    try:
+      response = await asyncio.wait_for(self.protocol.request(request).response, timeout=5.0)
+    except TimeoutError as e:
+      logger.error("[CoAPClient.FETCH] Failed to obtain FETCH response.")
+      raise e
+
+    if response:
+      logger.info("[CoAPClient.FETCH] Response obtained!")
+    return response
+
+async def main():
+  client = CoAPClient(CoAPBase.host, CoAPBase.port)
+  await client.init()
 
 if __name__=="__main__":
-  main()
+  asyncio.run(main())
