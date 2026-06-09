aabbf1b9a2e5d65710d5883649c83464636960b6
lroliver03 <lucas@oliver.adm.br>
2026-06-09 10:00:54 +0200

    Laid groundwork for unit tests

--- config.cmake
+++ config.cmake
@@ -1,9 +1,14 @@
+  # Project configurations
+
+set(PROJ_ENABLE_TESTS ON)
+set(PROJ_CLIENT ON)
+set(PROJ_SERVER ON)
+
   # libcoap configurations
 
-# Disable IPv4, TCP, DTLS(?) 
-# set(ENABLE_DTLS OFF)
-# set(ENABLE_TCP OFF)
-# set(ENABLE_IPV4 OFF)
+# Disable IPv4, TCP
+set(ENABLE_TCP OFF)
+set(ENABLE_IPV4 OFF)
 
 # Enable IPv6, CLIENT_MODE, SERVER_MODE, OSCORE (all on by default)
 set(ENABLE_IPV6 ON)
